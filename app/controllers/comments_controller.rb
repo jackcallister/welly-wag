a1d2dbcfdbbf1c4891091b15aec266304ffff3c8
Jack Callister <jarsbe@gmail.com>
2014-08-24 20:14:08 +1200

    Set up the comment controller.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: :show
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @comment = Comment.new( {user: current_user}.merge comment_params )
+
+    if @comment.save
+      redirect_to post_path(comment_params[:post_id]), notice: "Comment created successfully"
+    else
+      redirect_to post_path(comment_params[:post_id]), notice: "Something went wrong, please try again."
+    end
+  end
+
+  private
+
+  def comment_params
+    params[:comment].permit(
+      :content,
+      :post_id,
+      :comment_id
+    )
+  end
+end
