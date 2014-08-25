0a1862db25eec4e616137c57176280c45ca1edd3
Jack Callister <jarsbe@gmail.com>
2014-08-25 21:55:10 +1200

    Clear relevant notifications on post view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  after_action :clear_notifications!, only: :show, if: :current_user
 
   def index
     @posts = Kaminari.paginate_array(Post.ranked).page(params[:page]).per(10)
@@ -36,5 +37,11 @@ class PostsController < ApplicationController
       :url,
       :description
     )
+  end
+
+  def clear_notifications!
+    @post.comments.select { |c| c.notification }.each do |comment|
+      comment.notification.delete if comment.notification.recipient == current_user
+    end
   end
 end
