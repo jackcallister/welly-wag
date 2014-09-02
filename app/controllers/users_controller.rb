5494706b921d7e0b0b8ebb8bd3e65a761021a655
James McCann <jmccnz@gmail.com>
2014-09-02 21:56:24 +1200

    Added update method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
+  before_action :set_user
+
   def show
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Settings updated'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :send_digest)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
