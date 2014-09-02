bad39e17a2aed51ba1a19e35de3ce756ca00ad84
Jack Callister <jarsbe@gmail.com>
2014-09-02 18:16:54 +1200

    Add sender.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :notifiable, polymorphic: true
   belongs_to :recipient, class_name: 'User'
+  belongs_to :sender, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831031627) do
+ActiveRecord::Schema.define(version: 20140902060028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140831031627) do
     t.integer  "notifiable_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sender_id",       null: false
   end
 
   add_index "notifications", ["notifiable_id", "notifiable_type"], name: "index_notifications_on_notifiable_id_and_notifiable_type", using: :btree
